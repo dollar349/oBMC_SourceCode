f12bd47e36b9ba2c69030bff107b86f63829231a
“Dollar” <Dollar.Wang@vertivco.com>
2017-10-27 16:47:22 +0800

    Add phosphor-led-sysfs

--- get_source.sh
+++ get_source.sh
@@ -8,3 +8,4 @@ git clone https://github.com/openbmc/sdbusplus.git ${SOURCE_PATH}/sdbusplus
 git clone https://github.com/openbmc/skeleton.git ${SOURCE_PATH}/skeleton
 git clone https://github.com/openbmc/phosphor-host-ipmid.git ${SOURCE_PATH}/phosphor-host-ipmid
 git clone https://github.com/openbmc/phosphor-rest-server.git ${SOURCE_PATH}/phosphor-rest-server
+git clone https://github.com/openbmc/phosphor-led-sysfs.git ${SOURCE_PATH}/phosphor-led-sysfs
